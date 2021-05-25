a1f58f1033ee2aa6140c18964787638974a97fde
ehbeita <esteban.hernandez.beita@ucreativa.com>
2021-05-24 19:45:54 -0600

    Committing Main.java and created Esteban.java

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,9 +1,11 @@
 package com.ucreativa;
 
 import com.ucreativa.familia.Esteban;
+import com.ucreativa.familia.Marina;
 
 public class Main {
     public static void main (String[] args){
         Esteban esteban1 = new Esteban();
+        Marina marina1 = new Marina("Buenas noches");
     }
 }
---
+++ src/com/ucreativa/familia/Marina.java
@@ -0,0 +1,7 @@
+package com.ucreativa.familia;
+
+public class Marina {
+    public Marina(String saludo){
+        System.out.println(saludo);
+    }
+}
