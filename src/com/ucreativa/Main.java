298c8ed306afc0e8188bff35ee3c56f61aa1f8d6
ehbeita <esteban.hernandez.beita@ucreativa.com>
2021-06-03 19:23:34 -0600

    Committing BitacoraVacunas.java and Main.java with changes for new List.

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,11 +1,23 @@
 package com.ucreativa;
-
 import com.ucreativa.familia.Esteban;
 import com.ucreativa.familia.Marina;
+import com.ucreativa.vacunacion.BitacoraVacunas;
+import com.ucreativa.vacunacion.Persona;
+import java.util.Date;
+import java.util.ArrayList;
 
 public class Main {
     public static void main (String[] args){
         Esteban esteban1 = new Esteban("Buenas noches x2");
         Marina marina1 = new Marina("Buenas noches");
+        ArrayList BitacoraVacunas1 = new ArrayList();
+        Persona persona1 = new Persona("Esteban", "1-1536-0687",28,true);
+        Persona persona2 = new Persona("Marcela", "4-0293-1234",20,false);
+        BitacoraVacunas bitacora1 = new BitacoraVacunas(persona1,"Marca1",new Date());
+        BitacoraVacunas bitacora2 = new BitacoraVacunas(persona1,"Marca2",new Date());
+        BitacoraVacunas bitacora3 = new BitacoraVacunas(persona2,"Marca3",new Date());
+        BitacoraVacunas1.add(bitacora1);
+        BitacoraVacunas1.add(bitacora2);
+        BitacoraVacunas1.add(bitacora3);
     }
 }
---
+++ src/com/ucreativa/vacunacion/BitacoraVacunas.java
@@ -0,0 +1,28 @@
+package com.ucreativa.vacunacion;
+
+import java.util.Date;
+
+public class BitacoraVacunas{
+
+    private Persona persona;
+    private String marca;
+    private Date fecha;
+
+    public BitacoraVacunas(Persona persona, String marca, Date fecha){
+        this.persona=persona;
+        this.marca=marca;
+        this.fecha=fecha;
+    }
+
+    public Persona getPersona(){
+        return this.persona;
+    }
+
+    public String getMarca(){
+        return this.marca;
+    }
+
+    public Date getFecha(){
+        return this.fecha;
+    }
+}
